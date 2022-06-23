7c9c36e3faea4d0646c6d8c6a95a382fdfe6de5a
isabellacpmelo <isacpmelo@gmail.com>
2022-06-23 10:51:22 -0300

    Adição de novos exercicios

---
+++ repetições.js.js
@@ -0,0 +1,24 @@
+// Repetição com WHILE
+console.log('Vai começar #WHILE!!!')
+var a = 1
+while (a <= 3) {
+    console.log(`Ciclo ${a}`)
+    a++
+}
+console.log('FIM #WHILE!!!')
+
+// Repetição com DO WHILE
+console.log('Vai começar #DOWHILE!!!')
+var b = 1
+do {
+    console.log(`Ciclo ${b}`)
+    b++
+} while (b <= 3)
+console.log('FIM #DOWHILE!!!')
+
+// Repetição com FOR
+console.log('Vai começar #FOR!!')
+for (var c = 1; c <= 3; c++) {
+    console.log(`Passo ${c}`)
+}
+console.log('FIM #FOR!!!')
