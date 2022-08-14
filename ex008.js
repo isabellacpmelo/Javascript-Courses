121c137cad08f2944c16aee0166989c086269a38
isabellacpmelo <isacpmelo@gmail.com>
2022-08-14 15:05:53 -0300

    folder reorganization

--- ex008.js
+++
@@ -1,6 +0,0 @@
-var velocidade = 69
-console.log(`A velocidade do seu carro é ${velocidade}km/h.`)
-if (velocidade > 60) {
-    console.log('Você ultraassou a velocidade permitida! MULTADO!')
-}
-console.log('Dirija sempre usando cinto de segurança!')
